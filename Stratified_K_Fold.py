07fb16019a8ca752e6b369b6cedbbf2be6293b09
ncmentink <47645688+ncmentink@users.noreply.github.com>
2021-05-28 18:57:36 +0200

    update (mostly changing comments, cleaning)

--- Stratified_K_Fold.py
+++ Stratified_K_Fold.py
@@ -15,7 +15,6 @@ data = pd.read_csv("data.csv")
 X = data.drop('Bankrupt?', axis=1)
 y = data['Bankrupt?']
 
-
 def classification_report_with_f1(y_true, y_pred):
     true_labels.extend(y_true)
     predicted_labels.extend(y_pred)
@@ -44,13 +43,15 @@ def classification_report_with_roc_auc(y_true, y_pred):
     return roc_auc_score(y_true, y_pred)
 
 
-# C = 1/Lambda. By decreasing C, we increase sparsity and hence should get more zero predictions.
+# Choose a range of values for C = 1/Lambda.
+# By decreasing C, we increase sparsity and get more zero predictions.
 C = [10, 5, 1, 0.5, 0.1, 0.05, 0.001]
 
 output_file = open('classification_report.txt', 'w')
 
 for c in C:
-    # First model: Lasso (slow), second: Ridge (quick)
+    # First model: Lasso (l1), second: Ridge (l2)
+    # Increase no. of iterations to ensure convergence
     # Turn one off with # if desired
     Models = [LogisticRegression(penalty="l1", C=c, solver='saga', max_iter=8000),
               LogisticRegression(penalty="l2", C=c, solver='lbfgs', max_iter=8000)]
@@ -62,33 +63,26 @@ for c in C:
         predicted_labels = []
 
         # Over sample to a 1:10 ratio
-        over = SMOTE(sampling_strategy=0.1)
+        # alpha = (# in minority class / # in majority class after resampling)
+        over = SMOTE(sampling_strategy=0.1, random_state=0)
 
-        # Under sample to a 1:2 ratio
-        under = RandomUnderSampler(sampling_strategy=0.5)
+        # Under sample to a 1:2 ratio.
+        under = RandomUnderSampler(sampling_strategy=0.5, random_state=0)
 
-        # Pipelines help avoid leaking statistics from your test data into the trained model in cross-validation,
-        # by ensuring that the same samples are used to train the transformers and predictors. Also, the scaler is fit
-        # on the training data, transforms the train data, models are fitted on the train data, and the scaler is used
-        # to transform the test data. Therefore, the test data is not used to determine the scaling parameters.
-        #
-        # After under and over sampling we standardize the data, as this is necessary for Ridge and Lasso. If we
-        # standardize instead of normalize, we still keep the interpretability of our coefficients.
-        # For the Lasso, the regularization penalty is comprised of the sum of the absolute value of the coefficients,
-        # therefore we need to standardize the data so the coefficients are all based on the same scale.
-        #
-        # The folds of the cross-validation are stratified, which means they have the same class distribution as the
-        # original dataset, in this case a 1:32 ratio (3% 1-class, 97 0-class) and later 1:2 (33% 1-class, 66% 0-class).
-
-        steps = [('over', over), ('under', under), ('scale', StandardScaler()), ('model', model)]
+        # Folds are stratified; therefore have the same ratio as original dataset.
+        # Original: 1:32 ratio (3% 1-class, 97 0-class);
+        # After pipeline 1:2 (33% 1-class, 66% 0-class).
+        steps = [('over', over), ('under', under), ('scale', StandardScaler()),
+                 ('model', model)]
         pipeline = Pipeline(steps=steps)
 
         # Nested CV with parameter optimization
-        # Set measure to optimize by changing scoring function: accuracy, recall or f1-score
-        cross_val_score(pipeline, X, y, cv=StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
-                        scoring=make_scorer(classification_report_with_roc_auc))
+        # Pick optimization measure: accuracy, recall, f1-score
+        cross_val_score(pipeline, X, y, cv=StratifiedKFold(n_splits=10, shuffle=True,
+                                                           random_state=0),
+                        scoring=make_scorer(classification_report_with_recall))
 
-        # Average values in classification report for all folds in a Stratified K-fold Cross-validation
+        # Get average classification report of all K estimations
         print('C:', c)
         print('model:', model.penalty)
         print("ROC_AUC score:", roc_auc_score(true_labels, predicted_labels))

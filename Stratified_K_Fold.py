2a1137c80e1c42e2b1c313a5f6ebd5f91a89e157
Tobias Witte <tobias@aggatechnologies.com>
2021-06-04 12:53:32 +0200

    last push

--- Stratified_K_Fold.py
+++ Stratified_K_Fold.py
@@ -11,6 +11,14 @@ from imblearn.pipeline import Pipeline
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler
 
+
+import numpy as np
+
+variance = [65,68,68,68,68,68,68,69,69,69,69,70,70,70,70,70,70,70,70,70,70,70,70,70,70,70,70,70,70,70,71,71,71,71,71,71,71,71,71,71,71,72,72,72,72,72,73,73,73,73]
+print(len(variance))
+print(np.mean(variance))
+print(np.var(variance))
+
 # Load data
 data = pd.read_csv("data.csv")
 
@@ -52,54 +60,62 @@ C = [0.07]
 
 output_file = open('classification_report.txt', 'w')
 
-for c in C:
-    # First model: Lasso (l1), second: Ridge (l2)
-    # Increase no. of iterations to ensure convergence
-    # Turn one off with # if desired
-    Models = [LogisticRegression(penalty="l1", C=c, solver='saga', max_iter=8000),
-              LogisticRegression(penalty="l2", C=c, solver='lbfgs', max_iter=8000)]
 
-    for model in Models:
+recall0 = []
+for i in range(50):
 
-        # Create lists for the average classification report
-        true_labels = []
-        predicted_labels = []
+    for c in C:
 
-        # Over sample to a 1:10 ratio
-        over = SMOTE(sampling_strategy=0.1, random_state=3)
+        # First model: Lasso (l1), second: Ridge (l2)
+        # Increase no. of iterations to ensure convergence
+        # Turn one off with # if desired
+        # Models = [LogisticRegression(penalty="l1", C=c, solver='saga', max_iter=8000),
+        #           LogisticRegression(penalty="l2", C=c, solver='lbfgs', max_iter=8000)]
+        Models = [LogisticRegression(penalty="l1", C=c, solver='saga', max_iter=8000)]
 
-        # Under sample to a 1:2 ratio.
-        # alpha = (# in minority class / # in majority class after resampling)
-        under = RandomUnderSampler(sampling_strategy=0.5, random_state=3)
+        for model in Models:
 
-        # Folds are stratified; therefore have the same ratio as original dataset.
-        # Original: 1:32 ratio (3% 1-class, 97 0-class);
-        # After pipeline 1:2 (33% 1-class, 66% 0-class).
-        steps = [('over', over), ('under', under), ('scale', StandardScaler()),
-                 ('model', model)]
-        pipeline = Pipeline(steps=steps)
+            # Create lists for the average classification report
+            true_labels = []
+            predicted_labels = []
 
-        # Nested CV with parameter optimization
-        # Pick optimization measure: accuracy, recall, f1-score
-        cross_val_score(pipeline, X, y, cv=StratifiedKFold(n_splits=10, shuffle=True,
-                                                           random_state=0),
-                        scoring=make_scorer(classification_report_with_recall))
+            # Over sample to a 1:10 ratio
+            over = SMOTE(sampling_strategy=0.1, random_state=3)
 
-        # Get average classification report of all K estimations
-        print('C:', c)
-        print('model:', model.penalty)
-        print("ROC_AUC score:", roc_auc_score(true_labels, predicted_labels))
-        print(classification_report(true_labels, predicted_labels))
+            # Under sample to a 1:2 ratio.
+            # alpha = (# in minority class / # in majority class after resampling)
+            under = RandomUnderSampler(sampling_strategy=0.5, random_state=3)
 
-        output_file.write("C = %s, model =  %s \n" % (c, model.penalty))
-        output_file.write("ROC_AUC score: %s \n" % roc_auc_score(true_labels, predicted_labels))
-        output_file.write("%s\n" % classification_report(true_labels, predicted_labels))
+            # Folds are stratified; therefore have the same ratio as original dataset.
+            # Original: 1:32 ratio (3% 1-class, 97 0-class);
+            # After pipeline 1:2 (33% 1-class, 66% 0-class).
+            steps = [('over', over), ('under', under), ('scale', StandardScaler()),
+                     ('model', model)]
+            pipeline = Pipeline(steps=steps)
 
-        # Make ROC/AUC plot
-        fpr, tpr, _ = roc_curve(true_labels, predicted_labels)
-        auc = roc_auc_score(true_labels, predicted_labels)
-        plt.plot(fpr, tpr, label="data 1, auc=" + str(auc))
-        plt.legend(loc=4)
-        plt.show()
+            # Nested CV with parameter optimization
+            # Pick optimization measure: accuracy, recall, f1-score
+            cross_val_score(pipeline, X, y, cv=StratifiedKFold(n_splits=10, shuffle=True),
+                            scoring=make_scorer(classification_report_with_recall))
+
+            # Get average classification report of all K estimations
+            print('C:', c)
+            print('model:', model.penalty)
+            print("ROC_AUC score:", roc_auc_score(true_labels, predicted_labels))
+            print(classification_report(true_labels, predicted_labels))
+            recall0.append(classification_report_with_recall(true_labels,predicted_labels))
+            output_file.write("C = %s, model =  %s \n" % (c, model.penalty))
+            output_file.write("ROC_AUC score: %s \n" % roc_auc_score(true_labels, predicted_labels))
+            output_file.write("%s\n" % classification_report(true_labels, predicted_labels))
+
+            # Make ROC/AUC plot
+            fpr, tpr, _ = roc_curve(true_labels, predicted_labels)
+            auc = roc_auc_score(true_labels, predicted_labels)
+            plt.plot(fpr, tpr, label="data 1, auc=" + str(auc))
+            plt.legend(loc=4)
+            # plt.show()
 
 output_file.close()
+
+print(np.mean(recall0))
+print(np.var(recall0))
